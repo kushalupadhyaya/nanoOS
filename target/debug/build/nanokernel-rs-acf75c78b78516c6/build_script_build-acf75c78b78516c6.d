c5230359bce76bf42b3a69f5a7c9b237c74eb055
Kushal Upadhyaya <kushal.upadhyaya1@gmail.com>
2025-01-17 13:44:43 +0000

    Delete directory

--- target/debug/build/nanokernel-rs-acf75c78b78516c6/build_script_build-acf75c78b78516c6.d
+++
@@ -1,5 +0,0 @@
-/Users/Kushal/Desktop/Rust Nanokernel/target/debug/build/nanokernel-rs-acf75c78b78516c6/build_script_build-acf75c78b78516c6: build.rs
-
-/Users/Kushal/Desktop/Rust Nanokernel/target/debug/build/nanokernel-rs-acf75c78b78516c6/build_script_build-acf75c78b78516c6.d: build.rs
-
-build.rs:
